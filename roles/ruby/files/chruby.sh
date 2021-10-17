17e64c5cccab572d4e55a606da8dfe59a863320b
Jon Dufresne <jon.dufresne@gmail.com>
2021-10-17 10:37:40 -0700

    Remove unnecessary scripts

--- roles/ruby/files/chruby.sh
+++ roles/ruby/files/chruby.sh
@@ -1,7 +1,9 @@
+RUBY_VERSION_REGEXP="^ruby ([0-9]+\.[0-9]+\.[0-9]+)p[0-9]+$"
+
 auto-chruby() {
-    lockfile=$(gemfile-default-lockfile)
-    if [[ -n "${lockfile}" ]]; then
-        chruby $(gemfile-ruby-version "${lockfile}")
+    version=$(bundle platform --ruby 2> /dev/null)
+    if [[ ${version} =~ ${RUBY_VERSION_REGEXP} ]]; then
+        chruby "${BASH_REMATCH[1]}"
     else
         chruby system
     fi
