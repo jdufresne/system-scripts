66be39bc6d97c7b33194c757124ab192040ca632
Jon Dufresne <jon.dufresne@gmail.com>
2022-03-12 08:12:41 -0800

    Always use system bundler

--- roles/ruby/files/chruby.sh
+++ roles/ruby/files/chruby.sh
@@ -1,7 +1,7 @@
 RUBY_VERSION_REGEXP="^ruby ([0-9]+\.[0-9]+\.[0-9]+)p[0-9]+$"
 
 auto-chruby() {
-    version=$(bundle platform --ruby 2> /dev/null)
+    version=$(/usr/bin/bundle platform --ruby 2> /dev/null)
     if [[ ${version} =~ ${RUBY_VERSION_REGEXP} ]]; then
         chruby "${BASH_REMATCH[1]}"
     else
