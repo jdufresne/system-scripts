0120ccc2b8a9b0e7db905b662904d7a4deb44fa9
Jon Dufresne <jon.dufresne@gmail.com>
2024-01-28 13:04:43 -0800

    Quote aliases

--- roles/ruby/files/chruby.sh
+++ roles/ruby/files/chruby.sh
@@ -3,7 +3,7 @@
 . /usr/local/share/chruby/chruby.sh
 . /usr/local/share/chruby/auto.sh
 
-alias be=bundle exec
-alias rails=bundle exec rails
-alias rake=bundle exec rake
-alias rspec=bundle exec rspec
+alias be="bundle exec"
+alias rails="bundle exec rails"
+alias rake="bundle exec rake"
+alias rspec="bundle exec rspec"
