d84bb815ab025f851ab6e7e65368b4024a14c014
Jon Dufresne <jon.dufresne@gmail.com>
2024-01-28 12:56:42 -0800

    Add some aliases

--- roles/ruby/files/chruby.sh
+++ roles/ruby/files/chruby.sh
@@ -2,3 +2,8 @@
 
 . /usr/local/share/chruby/chruby.sh
 . /usr/local/share/chruby/auto.sh
+
+alias be=bundle exec
+alias rails=bundle exec rails
+alias rake=bundle exec rake
+alias rspec=bundle exec rspec
