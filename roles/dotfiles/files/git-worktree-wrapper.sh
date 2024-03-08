c4165402a80563df35cca8e045dc382f0af1920e
Jon Dufresne <jon.dufresne@gmail.com>
2024-03-08 08:33:18 -0800

    Add worktree wrapper

---
+++ roles/dotfiles/files/git-worktree-wrapper.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -eux
+
+function git-workon() {
+    if [ "$#" -ne 1 ]; then
+        printf "Wrong number of arguments (given %d expected 1)\n" "${#}" 1>&2
+        return 1
+    fi
+
+    branch="${1}"
+    git_root=$(git rev-parse --show-toplevel)
+
+    repos_base="${HOME}/devel/repos/"
+    worktrees_base="${HOME}/devel/worktrees/"
+
+    if [[ "${git_root}" =~ ^"${repos_base}" ]]; then
+        repo_dir="${git_root}"
+        worktree_dir="${worktrees_base}${repo_dir#"${repos_base}"}/${branch}"
+    elif [[ "${git_root}" =~ ^"${HOME}/devel/worktrees/" ]]; then
+        parent_dir=$(dirname "${git_root}")
+        repo_dir="${repos_base}${parent_dir#"${worktrees_base}"}"
+        worktree_dir="${parent_dir}/${branch}"
+    else
+        printf "Not in a project directory\n" 1>&2
+        return 1
+    fi
+
+    if [[ ! -d "${worktree_dir}" ]]; then
+        git worktree add -b "${branch}" "${worktree_dir}"
+    fi
+
+    cd "${worktree_dir}"
+}
+
+function git-workoff() {
+    if [ "$#" -ne 0 ]; then
+        printf "Wrong number of arguments (given %d expected 0)\n" "${#}" 1>&2
+        exit 1
+    fi
+
+    git_root=$(git rev-parse --show-toplevel)
+
+    repos_base="${HOME}/devel/repos/"
+    worktrees_base="${HOME}/devel/worktrees/"
+
+    if [[ "${git_root}" =~ ^"${HOME}/devel/worktrees/" ]]; then
+        branch=$(basename "${git_root}")
+        parent_dir=$(dirname "${git_root}")
+        repo_dir="${repos_base}${parent_dir#"${worktrees_base}"}"
+    else
+        printf "Not in a worktree directory\n" 1>&2
+        exit 1
+    fi
+
+    cd "${repo_dir}"
+
+    git worktree remove "${branch}"
+}
