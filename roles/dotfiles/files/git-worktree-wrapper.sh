73fa53ffa37c894a18a9e5a4d9a3bd9cf1a3ff68
Jon Dufresne <jon.dufresne@gmail.com>
2024-03-08 11:19:06 -0800

    Add convenience functions

--- roles/dotfiles/files/git-worktree-wrapper.sh
+++ roles/dotfiles/files/git-worktree-wrapper.sh
@@ -1,23 +1,31 @@
 #!/bin/bash -eux
 
+REPOS_BASE="${HOME}/devel/repos/"
+WORKTREES_BASE="${HOME}/devel/worktrees/"
+
+function repo() {
+    cd "${REPOS_BASE}/${1}"
+}
+
+function worktree() {
+    cd "${WORKTREES_BASE}/${1}"
+}
+
 function git-workon() {
     if [ "$#" -ne 1 ]; then
         printf "Wrong number of arguments (given %d expected 1)\n" "${#}" 1>&2
         return 1
     fi
 
-    repos_base="${HOME}/devel/repos/"
-    worktrees_base="${HOME}/devel/worktrees/"
-
     branch="${1}"
     git_root=$(git rev-parse --show-toplevel)
 
-    if [[ "${git_root}" =~ ^"${repos_base}" ]]; then
+    if [[ "${git_root}" =~ ^"${REPOS_BASE}" ]]; then
         repo_dir="${git_root}"
-        worktree_dir="${worktrees_base}${repo_dir#"${repos_base}"}/${branch}"
+        worktree_dir="${WORKTREES_BASE}${repo_dir#"${REPOS_BASE}"}/${branch}"
     elif [[ "${git_root}" =~ ^"${HOME}/devel/worktrees/" ]]; then
         parent_dir=$(dirname "${git_root}")
-        repo_dir="${repos_base}${parent_dir#"${worktrees_base}"}"
+        repo_dir="${REPOS_BASE}${parent_dir#"${WORKTREES_BASE}"}"
         worktree_dir="${parent_dir}/${branch}"
     else
         printf "Not in a project directory\n" 1>&2
@@ -39,13 +47,10 @@ function git-workoff() {
 
     git_root=$(git rev-parse --show-toplevel)
 
-    repos_base="${HOME}/devel/repos/"
-    worktrees_base="${HOME}/devel/worktrees/"
-
     if [[ "${git_root}" =~ ^"${HOME}/devel/worktrees/" ]]; then
         branch=$(basename "${git_root}")
         parent_dir=$(dirname "${git_root}")
-        repo_dir="${repos_base}${parent_dir#"${worktrees_base}"}"
+        repo_dir="${REPOS_BASE}${parent_dir#"${WORKTREES_BASE}"}"
     else
         printf "Not in a worktree directory\n" 1>&2
         exit 1
